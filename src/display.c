398a517d4705501f2dee8405c72b29f59919996e
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-09-30 09:29:06 -0500

    Refactored main.c to hold some code in display.h and display.c

---
+++ src/display.c
@@ -0,0 +1,85 @@
+#include "display.h"
+
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+SDL_Texture* color_buffer_texture = NULL;
+int window_width = 800;
+int window_height = 600;
+
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		fprintf(stderr, "Error Initializing SDL.\n");
+		return false;
+	}
+
+	// Query for maximum monitor resolution
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
+	// Create SDL Window	
+	window = SDL_CreateWindow(
+		NULL,                        // Window Title, NULL for no name
+		SDL_WINDOWPOS_CENTERED,      // Where the window will show up on monitor displayed
+		SDL_WINDOWPOS_CENTERED,      // WINDOWPOS_CENTERED for center of monitor
+		window_width,                // Window Width
+		window_height,               // Window Height
+		SDL_WINDOW_BORDERLESS 		 // Any flags for display window, Borderless in this case
+	);
+	if (!window) {
+		fprintf(stderr, "Error Creating SDL Window.\n");
+		return false;
+	}
+	
+	// Create SDL Renderer
+	renderer = SDL_CreateRenderer(
+		window,                      // Pass in window object for the renderer to use
+		-1,                          // Get monitor device to render on to, -1 for choose default
+		0                            // Flags, 0 for leave everything as default
+	);
+	if (!renderer) {
+		fprintf(stderr, "Error Creating SDL Renderer.\n");
+		return false;
+	}
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+	
+	return true;
+}
+
+void render_color_buffer(void) {
+	SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		(int)(window_width * sizeof(uint32_t))
+	);
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+	for (int y = 0; y < window_height; y++) {
+		for (int x = 0; x < window_width; x++) {
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			int current_x = x + i;
+			int current_y = y + j;
+			color_buffer[(window_width * current_y) + current_x] = color;
+		}
+	}
+}
+
+void destroy_window(void) {
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
--- src/main.c
+++ src/main.c
@@ -2,58 +2,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <SDL2/SDL.h>
+#include "display.h"
 
 bool is_running = false;
-
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-uint32_t* color_buffer = NULL;
-SDL_Texture* color_buffer_texture = NULL;
-
-int window_width;
-int window_height;
-
-bool initialize_window(void) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error Initializing SDL.\n");
-		return false;
-	}
-
-	// Query for maximum monitor resolution
-	SDL_DisplayMode display_mode;
-	SDL_GetCurrentDisplayMode(0, &display_mode);
-	window_width = display_mode.w;
-	window_height = display_mode.h;
-
-	// Create SDL Window	
-	window = SDL_CreateWindow(
-		NULL,                        // Window Title, NULL for no name
-		SDL_WINDOWPOS_CENTERED,      // Where the window will show up on monitor displayed
-		SDL_WINDOWPOS_CENTERED,      // WINDOWPOS_CENTERED for center of monitor
-		window_width,                // Window Width
-		window_height,               // Window Height
-		SDL_WINDOW_BORDERLESS 		 // Any flags for display window, Borderless in this case
-	);
-	if (!window) {
-		fprintf(stderr, "Error Creating SDL Window.\n");
-		return false;
-	}
-	
-	// Create SDL Renderer
-	renderer = SDL_CreateRenderer(
-		window,                      // Pass in window object for the renderer to use
-		-1,                          // Get monitor device to render on to, -1 for choose default
-		0                            // Flags, 0 for leave everything as default
-	);
-	if (!renderer) {
-		fprintf(stderr, "Error Creating SDL Renderer.\n");
-		return false;
-	}
-	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-	
-	return true;
-}
 
 void setup(void) {
 	// Allocate memory to hold color buffer
@@ -94,39 +45,16 @@ void update(void) {
 	// TODO
 }
 
-void render_color_buffer(void) {
-	SDL_UpdateTexture(
-		color_buffer_texture,
-		NULL,
-		color_buffer,
-		(int)(window_width * sizeof(uint32_t))
-	);
-	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void clear_color_buffer(uint32_t color) {
-	for (int y = 0; y < window_height; y++) {
-		for (int x = 0; x < window_width; x++) {
-			color_buffer[(window_width * y) + x] = color;
-		}
-	}
-}
-
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);  // Put render color onto display window over everything that has been rendered
 
+	draw_rect(200, 200, 200, 100, 0xFFFF00FF);
+
 	render_color_buffer();	
-	clear_color_buffer(0xFFFFFF00);
+	clear_color_buffer(0xFF000000);
 	
 	SDL_RenderPresent(renderer);
-}
-
-void destroy_window(void) {
-	free(color_buffer);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 }
 
 int main(void) {
