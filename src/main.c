87a310c65f49258fce73a75d6af455de8d781b45
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-10-01 17:26:50 -0500

    Setup vector3 and vector2 structs

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 #include <stdint.h>
 #include <SDL2/SDL.h>
 #include "display.h"
+#include "vector.h"
+
+// Declare array of vectors/points
+#define N_POINTS (9 * 9 * 9) 
+vec3_t cube_points[N_POINTS]; // 9x9x9 cube
 
 bool is_running = false;
 
@@ -23,6 +28,19 @@ void setup(void) {
 	);
 	if (!color_buffer_texture) {
 		fprintf(stderr, "Error Creating Color Buffer Texture.\n");
+	}
+
+	// Start loading array of vectors
+	// From -1 to 1 in 9x9x9 cube
+	int point_count = 0;
+
+	for (float x = -1; x <= 1; x += 0.25) {
+		for (float y = -1; y <= 1; y += 0.25) {
+			for (float z = -1; z <= 1; z += 0.25) {
+				vec3_t new_point = { .x = x, .y = y, .z = z };
+				cube_points[point_count++] = new_point;
+			}
+		}
 	}
 }
 
---
+++ src/vector.c
@@ -0,0 +1,4 @@
+#include "vector.h"
+
+// TODO: Implementation of vector functions
+
