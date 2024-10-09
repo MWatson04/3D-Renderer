06360f4e67220804df4f822dc14b8085e0df277a
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-10-09 15:51:04 -0500

    Implemented function that calculates dot product

--- src/vector.h
+++ src/vector.h
@@ -15,6 +15,7 @@ vec2_t vec2_add(vec2_t a, vec2_t b);            // ADDITION
 vec2_t vec2_sub(vec2_t a, vec2_t b);            // SUBTRACTION
 vec2_t vec2_mul(vec2_t v, float factor);        // MULTIPLICATION
 vec2_t vec2_div(vec2_t v, float factor);        // DIVISION
+float vec2_dot(vec2_t a, vec2_t b);             // DOT PRODUCT
 
 // Vector 3D functions
 float vec3_length(vec3_t v);
@@ -23,6 +24,7 @@ vec3_t vec3_sub(vec3_t a, vec3_t b);            // SUBTRACTION
 vec3_t vec3_mul(vec3_t v, float factor);        // MULTIPLICATION
 vec3_t vec3_div(vec3_t v, float factor);        // DIVISION
 vec3_t vec3_cross(vec3_t a, vec3_t b);          // CROSS PRODUCT
+float vec3_dot(vec3_t a, vec3_t b);             // DOT PRODUCT
 
 void vec3_rotate_x(vec3_t* v, float angle);
 void vec3_rotate_y(vec3_t* v, float angle);
