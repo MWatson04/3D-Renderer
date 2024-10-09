14f78b553ef9e5744058f3e1862a424ce1304a9f
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-10-09 13:53:56 -0500

    Implemented functions for vector operations (add, sub, mul, div)

--- src/vector.h
+++ src/vector.h
@@ -2,15 +2,26 @@
 #define VECTOR_H
 
 typedef struct {
-    float x;
-    float y;
+    float x, y;
 } vec2_t;
 
 typedef struct {
-    float x;
-    float y;
-    float z;
+    float x, y, z;
 } vec3_t;
+
+// Vector 2D functions
+float vec2_length(vec2_t v);
+vec2_t vec2_add(vec2_t a, vec2_t b);            // ADDITION
+vec2_t vec2_sub(vec2_t a, vec2_t b);            // SUBTRACTION
+vec2_t vec2_mul(vec2_t v, float factor);        // MULTIPLICATION
+vec2_t vec2_div(vec2_t v, float factor);        // DIVISION
+
+// Vector 3D functions
+float vec3_length(vec3_t v);
+vec3_t vec3_add(vec3_t a, vec3_t b);            // ADDITION
+vec3_t vec3_sub(vec3_t a, vec3_t b);            // SUBTRACTION
+vec3_t vec3_mul(vec3_t v, float factor);        // MULTIPLICATION
+vec3_t vec3_div(vec3_t v, float factor);        // DIVISION
 
 void vec3_rotate_x(vec3_t* v, float angle);
 void vec3_rotate_y(vec3_t* v, float angle);
