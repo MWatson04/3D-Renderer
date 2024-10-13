6a01254f3d700ff78ab4122b7f71147519cfde2f
MWatson04 <91926425+MWatson04@users.noreply.github.com>
2024-10-13 15:00:47 -0500

    Corrected possible division by zero error when calling draw_filled_triangle function

--- src/display.c
+++ src/display.c
@@ -3,8 +3,10 @@
 
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-uint32_t* color_buffer = NULL;
 SDL_Texture* color_buffer_texture = NULL;
+
+uint32_t* color_buffer = NULL;
+
 int window_width = 800;
 int window_height = 600;
 
